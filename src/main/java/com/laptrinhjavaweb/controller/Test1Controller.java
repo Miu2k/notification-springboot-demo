da3ed8a40970e7af8c9834d1512c735f3d4c4558
BVCN 88 <thaoluong5343>
2022-05-11 23:15:56 +0700

    test

---
+++ src/main/java/com/laptrinhjavaweb/controller/Test1Controller.java
@@ -0,0 +1,29 @@
+package com.laptrinhjavaweb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class Test1Controller {
+
+    @GetMapping("/123")
+    public String index(Model model) {
+        model.addAttribute("title", "Trang chủ");
+        return "index";
+    }
+
+    @GetMapping("/send-notification123")
+    public String sendNotiPage(Model model) {
+        model.addAttribute("title", "Gửi thông báo");
+        return "send-notification";
+    }
+
+    @GetMapping("/send-mail123")
+    public String sendMail(Model model) {
+        model.addAttribute("title","Gửi mail");
+        return "mail";
+    }
+}
+
